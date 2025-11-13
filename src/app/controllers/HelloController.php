a77ca506278eca8e84b1d6aa88f3d07789f50846
ikerarana <iarana@birt.eus>
2025-11-13 13:22:52 +0100

    añadidios el HomeController

---
+++ src/app/controllers/HelloController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Core\BaseController;
+
+final class HelloController extends BaseController
+{
+    // GET /
+    public function index(): void
+    {
+        $this->json([
+            'ok'        => true,
+            'service'   => 'api-pruebas-despliegue',
+            'endpoints' => ['/prueba'],
+        ]);
+    }
+
+    // GET /prueba
+    public function hello(): void
+    {
+        $this->json([
+            'message' => 'Hola desde la API estructurada 👋',
+        ]);
+    }
+}
